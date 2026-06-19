5bf09c9de2f60e81616f82a5c5555e2b2a44e952
1234Vishnu <viratvishnu04@gmail.com>
2026-06-19 17:06:46 +0530

    Initial Commit

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
 
-app.use("/api", (req, res) => {
-  res.send("Hello from the API!");
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
 });
-app.use("/", (req, res) => {
-  res.send("Hello, World!");
+
+app.get('/hello', (req, res) => {
+    res.send('Hello from the server');
 });
+app.get('/learn', (req, res) => {
+    res.send('Learning Node.js is fun!');
+});
+
 app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+    console.log('Server is running on port 3000');
 });
