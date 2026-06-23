9a5ee056d845265e3e456602b625b1e3f418b369
1234Vishnu <viratvishnu04@gmail.com>
2026-06-23 15:46:32 +0530

    Multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,11 +2,34 @@ const express = require('express');
 
 const app = express();
 
-// Reading query parameters 
-app.get('/user/:id/:name/:password', (req, res) => {
-    console.log(req.params);
-    res.send('GET User route');
-});
+// Multiple Route Handlers
+app.get('/user', (req, res, next) => {
+    console.log('GET User route1');
+    // res.send('GET User route1');
+    next(); // Pass control to the next handler
+},
+    (req, res, next) => {
+        console.log('GET User route2');
+        //res.send('GET User route2')
+        next(); // Pass control to the next handler
+    },
+    (req, res, next) => {
+        console.log('GET User route3');
+        // res.send('GET User route3');
+        next(); //Pass the control to the next handler
+    },
+    (req, res, next) => {
+        console.log('GET User route4');
+        // res.send('GET User route4');
+        next(); //Pass the control to the next handler
+    },
+    (req, res, next) => {
+        console.log('GET User route5');
+        res.send('GET User route5');
+        next(); //Pass the control to the next handler
+    }
+);
+
 
 
 app.listen(3000, () => {
