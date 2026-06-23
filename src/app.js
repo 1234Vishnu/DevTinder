836056e27aeb7cd5a8a2f01bd7e00fc1aca83ed8
1234Vishnu <viratvishnu04@gmail.com>
2026-06-23 14:47:37 +0530

    Reading query params

--- src/app.js
+++ src/app.js
@@ -2,32 +2,12 @@ const express = require('express');
 
 const app = express();
 
-//This will match the GET, POST, PATCH, and DELETE methods for the /user route
-app.get('/user', (req, res) => {
-    console.log('GET User route');
+// Reading query parameters 
+app.get('/user/:id/:name/:password', (req, res) => {
+    console.log(req.params);
     res.send('GET User route');
 });
 
-app.post('/user', (req, res) => {
-    console.log('POST User route');
-    res.send('POST User route');
-});
-
-app.patch('/user', (req, res) => {
-    console.log('PATCH User route');
-    res.send('PATCH User route');
-});
-
-app.delete('/user', (req, res) => {
-    console.log('DELETE User route');
-    res.send('DELETE User route');
-});
-
-// This will match all the HPP methods for the /user route
-app.use('/user', (req, res) => {
-    console.log('Fallback User route');
-    res.send('User route');
-});
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
