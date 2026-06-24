5c49e61b82adc26d8e1789b7fb73a8a8ab7be543
1234Vishnu <viratvishnu04@gmail.com>
2026-06-24 15:57:35 +0530

    Error Handling

--- src/app.js
+++ src/app.js
@@ -1,27 +1,26 @@
 const express = require('express');
-const { adminAuth } = require('./middlewares/Auth');
-const { userAuth } = require('./middlewares/Auth');
 const app = express();
 
-app.use('/admin', adminAuth);
-
-
-app.get('/admin/getAllData', (req, res) => {
-    res.send('Get all admin data');
+app.use('/', (err, req, res, next) => {
+  if (err) {
+    res.status(500).send('Something went wrong from "/1');
+  }
 });
 
-app.get('/admin/getUserData', (req, res) => {
-    res.send('Get user data for admin');
+app.get('/getUserData', (req, res) => {
+  try {
+    throw new Error('Simulated error in /getUserData route');
+  } catch (err) {
+    res.status(500).send('Something went wrong');
+  }
 });
 
-app.get('/user/getProfile', userAuth, (req, res) => {
-    res.send('Get user profile');
-});
-
-app.post('/user/login', (req, res) => {
-    res.send('User logedin successfully');
+app.use('/', (err, req, res, next) => {
+  if (err) {
+    res.status(500).send('Something went wrong from "/2');
+  }
 });
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  console.log('Server is running on port 3000');
 });
